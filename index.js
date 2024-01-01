30922e0d172d01d16f803c968d8d34f37c70fc89
ramune314159265 <ramune314159265@gmail.com>
2024-01-01 22:08:01 +0900

    keyboardUI実装

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
+class EventRegister {
+    constructor() { this.events = {} }
+    on(name, func) {
+        if (this.events[name] === undefined) this.events[name] = new Array()
+        this.events[name].push(func)
+    }
+    emit(name, ...arg) {
+        if (this.events[name] === undefined) return
+        this.events[name].forEach(fn => fn(...arg))
+    }
+}
+
+class KeyboardUI extends EventRegister {
+    constructor(layout) {
+        super()
+        const layouts = {
+            'calc': document.querySelector('#calcKeyboard')
+        }
+        this.component = layouts[layout].content.cloneNode(true);
+        this.component.childNodes.forEach(node => {
+            node.addEventListener('click', e => {
+                if (!e.target.dataset.key) {
+                    return
+                }
+                this.emit('keypress', e.target.dataset.key)
+            })
+        })
+    }
+}
+
 class PIGameBase {
     constructor(numericalSequence) {
         this.numericalSequence = numericalSequence
@@ -8,8 +38,15 @@ class PIGameBase {
     }
 }
 
-class PracticeMode extends PIGameBase{
-    constructor(numericalSequence){
+class PracticeMode extends PIGameBase {
+    constructor(numericalSequence) {
         super(numericalSequence);
     }
 }
+
+const test = document.createElement('div')
+test.classList.add('keyboard')
+const keyboardUI = new KeyboardUI('calc')
+test.appendChild(keyboardUI.component)
+document.body.appendChild(test)
+keyboardUI.on('keypress', console.log)
