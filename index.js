cd7a4c120dc2a0cf2d5b7cfb6915cd0b93e14ca2
Ramune <121664026+ramune314159265@users.noreply.github.com>
2024-01-08 14:59:21 +0900

    ボタン設定

--- index.js
+++ index.js
@@ -222,4 +222,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#practiceMode').addEventListener('click', () => {
         new PracticeMode('14159265358979323846264338327950288419716939937510582097494459230781640628620899862').show()
     })
+    document.querySelector('#challengeMode').addEventListener('click', () => {
+        new ChallengeMode('14159265358979323846264338327950288419716939937510582097494459230781640628620899862').show()
+    })
 })
