7c4755d15ae17e7dbf6b0ad64644780b56f8fbad
ramune314159265 <ramune314159265@gmail.com>
2024-01-17 17:46:21 +0900

    移動

--- src/js/loaded.js
+++ src/js/loaded.js
@@ -1,39 +1,5 @@
-import { MemorizeMode, PracticeMode, ChallengeMode } from './gamemodes/index.js'
-import { getConfig } from './util/config.js'
+import MainMenu from "./UI/menu.js"
 
-export const loaded = () => {const selector = document.querySelector('#sequenceSelector')
-    getConfig().mathSequences.forEach?.((sequence, i) => {
-        const optionElement = document.createElement('option')
-        optionElement.value = i
-        optionElement.textContent = sequence.name
-
-        selector.appendChild(optionElement)
-    });
-
-    const sequenceSelected = () => {
-        const index = selector.value
-        const sequence = getConfig().mathSequences[index]
-
-        const symbolElement = document.querySelector('.symbol')
-        MathJax.typesetPromise().then(() => {
-            symbolElement.textContent = `$$${sequence.tex}$$`
-            MathJax.typesetPromise()
-        })
-    }
-    selector.addEventListener('change', sequenceSelected)
-    sequenceSelected()
-
-    const gameStart = (GameMode, sequence = getConfig().mathSequences[selector.value].sequence) => {
-        new GameMode(sequence).show()
-    }
-    document.querySelector('#memorizeMode').addEventListener('click', () => {
-        gameStart(MemorizeMode)
-    })
-    document.querySelector('#practiceMode').addEventListener('click', () => {
-        gameStart(PracticeMode)
-    })
-    document.querySelector('#challengeMode').addEventListener('click', () => {
-        gameStart(ChallengeMode)
-    })
+export const loaded = () => {
+    new MainMenu().show()
 }
-
