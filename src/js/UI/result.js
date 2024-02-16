339e674627b9a7d49698ae6d69e0b5cb65452fcb
ramune314159265 <ramune314159265@gmail.com>
2024-02-16 19:13:33 +0900

    結果画面追加(途中)

---
+++ src/js/UI/result.js
@@ -0,0 +1,36 @@
+import { EventRegister } from '../util/eventRegister.js'
+
+export default class GameMainUI extends EventRegister {
+	constructor() {
+		super()
+		this.component = document.createElement('div')
+		this.component.classList.add('resultUI')
+		this.component.appendChild(document.querySelector('#resultUI').content.cloneNode(true))
+	}
+	async show() {
+		document.body.appendChild(this.component)
+		this.emit('showed')
+
+		await this.component.animate(
+			[
+				{ opacity: 0 },
+				{ opacity: 1 }
+			], {
+			duration: 200,
+			easing: 'ease-in-out',
+		}).finished
+	}
+	async hide() {
+		await this.component.animate(
+			[
+				{ opacity: 1 },
+				{ opacity: 0 }
+			], {
+			duration: 200,
+			easing: 'ease-in-out',
+			fill: 'forwards',
+		}).finished
+
+		this.component.remove()
+	}
+}
